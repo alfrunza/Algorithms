09e13d8bdb1e74d092165af098bcd4e487249801
afrunza1_elcomp <afrunza1@estee.com>
2024-01-31 15:29:35 +0200

    Put main method in Main class and reformatted code.

--- src/others/playlistRandomizer/Main.java
+++ src/others/playlistRandomizer/Main.java
@@ -1,10 +1,25 @@
 package others.playlistRandomizer;
 
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.List;
+
+import static others.playlistRandomizer.SongService.buildPlaylist;
+
 public class Main {
 
-
-
     public static void main(String[] args) {
-
+        final String fileName = "songs.json";
+        List<Song> playlist;
+        System.out.println("Your playlist:");
+        try {
+            playlist = buildPlaylist(fileName);
+            for (Song song : playlist) {
+                System.out.println(song.getTitle() + " by " + song.getArtist() + ". It's from the " + song.getAlbum() + " album.");
+            }
+        } catch (IOException | ParseException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
