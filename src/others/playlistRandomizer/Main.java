efaf3803ecd33cc15dad3d59b86bfaae193d9a07
afrunza1_elcomp <afrunza1@estee.com>
2024-02-06 15:01:30 +0200

    Optimized error handling

--- src/others/playlistRandomizer/Main.java
+++ src/others/playlistRandomizer/Main.java
@@ -5,20 +5,23 @@ import org.json.simple.parser.ParseException;
 import java.io.IOException;
 import java.util.List;
 
-import static others.playlistRandomizer.SongService.buildPlaylist;
+//Determine top 3 artists in the final playlist (um. %, two digits)
+//Determine top 5 albums in the final playlist (um. %, two digits)
+//Add menu for choosing what top to be displayed in the console - read from console
+
 
 public class Main {
 
     public static void main(String[] args) {
         final String fileName = "songs.json";
         List<Song> playlist;
-        System.out.println("Your playlist:");
         try {
-            playlist = buildPlaylist(fileName);
+            playlist = SongService.buildPlaylist(fileName);
+            System.out.println("Your playlist:");
             for (Song song : playlist) {
                 System.out.println(song.getTitle() + " by " + song.getArtist() + ". It's from the " + song.getAlbum() + " album.");
             }
-        } catch (IOException | ParseException e) {
+        } catch (IOException | ParseException | NullPointerException e) {
             System.out.println(e.getMessage());
         }
     }
